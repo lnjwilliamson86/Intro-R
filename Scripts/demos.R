c083df43b11142b3e675279775fbf47d9e92f002
Williamson <wil9e9@csiro.au>
2019-09-20 09:38:21 +1000

    inital commit of demos scripts mathematical operators in R

---
+++ Scripts/demos.R
@@ -0,0 +1,75 @@
+library(tidyverse)
+1+100
+
+3+5*2
+
+(3+5)*2
+
+3+5*2^2
+2/10000
+5e3 #demostrating scientific notation
+
+#equality
+
+1==1
+1!=2
+1!=1
+1<2
+1>10
+2<=5
+2<=2
+
+pi ==3.141593
+
+
+x<-1/40
+x
+y<-1000
+x+y
+rm(x)
+x
+y<-10
+log(x)
+log(1/40)
+log(0.025)
+
+
+x<-x+1
+x
+X
+x<-NA
+x
+x==NA
+is.na(x)
+my_data<-1
+my_data
+min_height<-1
+max.height<-1000
+_age<-30
+.mass<-75
+MaxLength<-67
+min-length<-35
+2widths<-23
+celsius2kelvin<-123
+
+
+mass <- 47.5
+age <- 122
+mass <- mass * 2.3
+age <- age - 20
+
+mass>age
+
+1:5
+2^(1:5)
+my_vector<-c(4,2,7,2,3)
+2^my_vector
+
+ls()
+ls()
+rm(max.height)
+ls()
+rm(my_data)
+ls()
+installed.packages()
+installed.packages("tidyverse")
