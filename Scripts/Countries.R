41a0db113b40a23c6b44b796103b7c365dc9094f
Williamson <wil9e9@csiro.au>
2019-09-20 16:32:10 +1000

    end of day 2 commit summarise arrange read excel write csv

--- Scripts/Countries.R
+++ Scripts/Countries.R
@@ -124,3 +124,98 @@ summarise(
   mean_pop = mean(pop),
   median_pop = median(pop)
 )#returns the mean and median pop in a new dataframe. 
+
+summarise_if(gapminder,is.numeric,mean)# returns new data frame for mean of  numeric cols 
+
+group_by(gapminder,country) #groups a data set by a col in this example 142 different counrty groups are created in this data set. 
+by_country <- group_by(gapminder,country) #groupings don't give a new dataframe but adds groups as a piece of information about the data frame
+
+summarise(gapminder,mean_pop=mean(pop),median_pop=median(pop))
+
+summarise(by_country,mean_pop=mean(pop),median_pop=median(pop)) #returns a new dataframe that gives mean pop for each grouping
+
+by_continent<- group_by(gapminder,continent) #returns dataframe grouped by continent and assigns to by_continent
+
+summarise(by_continent,mean_pop=mean(pop), median_pop= median(pop)) #returns mean and median for by_continent
+
+summarise(group_by(gapminder,continent),mean_pop=mean(pop),median_pop=median(pop)) #nested function for rows 137 and 139. Returns mean pop and median pop grouped by continent
+
+gapminder %>%
+  group_by(continent) %>%
+  summarise(mean_pop= mean(pop),median_pop= median(pop)) #piped function for 137-139 returns mean pop and median pop by continent
+
+#arrange
+arrange(gapminder,gdpPercap) #sorts data by acscending gdpPercap
+
+arrange(gapminder,desc(gdpPercap)) #uses helper function desc to organise in descending order by gdpPercap
+
+#Calculate the average life expectancy per country. Which has the shortest average life expectancy and which has the longest average life expectancy?
+mean_lifeExp_by_country <- gapminder %>% #get gapminder data and asgin a variable
+  group_by(country)%>% #group by country
+  summarise(mean_life_exp = mean(lifeExp))%>% #mean life exp per group
+  arrange(mean_life_exp) #order results ascending
+arrange(mean_lifeExp_by_country,desc(mean_life_exp)) #order data descending
+
+mean_lifeExp_by_country <- gapminder %>% #get gapminder data and asgin a variable
+  group_by(country)%>% #group by country
+  summarise(mean_life_exp = mean(lifeExp))%>% #mean life exp per group
+  filter(mean_life_exp==min(mean_life_exp)|mean_life_exp == max(mean_life_exp)) #returns just the min and max mean life exp
+
+summarise(gapminder,num_rows = n ())# returns the number of rows in the gapminder dataframe
+
+counts <- summarise(by_country,num_rows = n())#returns the number of rows for each country assigned counts so view function can be used in the console to open the data set
+view(summarise(by_country,num_rows = n())) # alternative to row 166
+summarise(by_country,num_rows = n())%>% # another alternative to 166
+  view
+
+#read in excel files
+library(readxl) # when reading in excel files need to use the readxl library beucase it is not in the core tidyverse
+
+gapminder_excel <- read_excel("data/gapminder.xlsx")#reads excel file and assigns 
+
+read_excel("data/gapminder.xlsx",range ="A1:E4") # to read only a range from an excel file from the first sheet
+
+read_excel("data/gapminder.xlsx",sheet = "gapminder") # to read only a particualr sheet in an excel workbook
+
+#write data out
+write_csv(gapminder_excel,"results/gapminder_output.csv") #writes out a csv file 
+write_tsv(gapminder_excel,"results/gapminder-output.tsv") #writes out a tsv file
+
+#jsut Australian data from gapminder
+gapminder%>% #call gapminder dataframe then
+  filter(country=="Australia")%>% #filter by country then
+  write_csv("results/australia_only.csv") #write to csv
+
+#see construct an analysis challenge
+gapminder%>% #read gapminder
+  filter(year==1972)%>% #filter to only 1972
+  arrange(desc(pop))%>% #sort by popualtion size in desc order
+  slice(6) #filter the 6th highest population row
+
+#create a filtered data frame for 1987
+life_exp_1987<- gapminder%>% #readgapminder
+  filter(year==1987)%>% #filter data to include 1987
+  arrange(desc(lifeExp))%>% #sort by life exp in desc order
+  slice(1:10) #slice the top 10 life expectancies
+life_exp_1987
+
+#create a filtered data frame for 2007
+life_exp_2007 <- gapminder %>% #readgapminder
+  filter(year==2007) %>% #filter data to include 2007
+  arrange(desc(lifeExp)) %>% #sort by life exp in desc order
+  slice(1:10) #slice the top 10 life exp 
+life_exp_2007
+#combine 1987 & 2007 data frames 
+#compare the data frames to see which countires are in both
+
+#class disscussion
+gapminder%>% #start with gapminder
+  filter(year==1987|year ==2007) %>% #just work woth data from 1987 and 2007
+  group_by(year) %>%  #year
+  arrange(desc(lifeExp)) %>% #sort data by life expectancy (by year)
+  top_n(10) %>% #get just the top ten (by year)
+  group_by(country) %>%  #country
+  summarise(country_life_exp=n()) %>%  #count the number of times a country appears (if appers 2 times then these are in both lists)
+  filter(country_life_exp ==2)%>% #filter on countries with a count of 2
+  write_csv("results/top10_life_exp_1987_and_2007") #write results to csv
+ 
